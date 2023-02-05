d90b1f9d6d64f158d413585fb81ecbc497d98c7c
Cleyson Lima <cleysonph@gmail.com>
2023-02-05 17:06:26 -0300

    create the Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import PageTitle from "@components/data-display/PageTitle";
+import { Button, Icon, TextField } from "@mui/material";
+import { BoxButtons, HomeContainer } from "@styles/pages/index.styles";
 
 export default function Home() {
   return (
-    <div>
-      <PageTitle title="HIPER PROF" subtitle="subtitulo" />
-    </div>
+    <HomeContainer>
+      <PageTitle
+        title="Encontre o professor ideal para você!"
+        subtitle="Pesquise pelo professor ideal para você."
+      />
+
+      <TextField
+        sx={{ mt: 3, mb: 1 }}
+        label={"Encontre um Professor"}
+        InputProps={{
+          startAdornment: <Icon>search</Icon>,
+        }}
+        fullWidth
+      />
+
+      <BoxButtons>
+        <Button variant="contained" type="submit">
+          Buscar o professor perfeito
+        </Button>
+      </BoxButtons>
+    </HomeContainer>
   );
 }
